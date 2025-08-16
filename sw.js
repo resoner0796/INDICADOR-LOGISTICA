02b0d0ee4e0ba34894557b0846ac6dcfe39ad0fe
resoner0796 <ulisees.luna96@gmail.com>
2025-08-16 16:47:42 -0500

    Actualizar sw.js

--- sw.js
+++ sw.js
@@ -3,8 +3,8 @@ const FILES_TO_CACHE = [
   './',
   './index.html',
   './manifest.json',
-  './icon-192.png',
-  './icon-512.png'
+  './icon-192.PNG',
+  './icon-512.PNG'
 ];
 
 self.addEventListener('install', e => {
